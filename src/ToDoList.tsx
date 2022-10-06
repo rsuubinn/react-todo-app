770288fda55c3d228a466b798780aa65cd544054
rsuubinn <rsuubinn@gmail.com>
2022-10-07 00:20:48 +0900

    #5.6 Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setToDo(event.currentTarget.value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(register("toDo"));
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="오늘의 할일"
-        ></input>
+      <form>
+        <input {...register} placeholder="오늘의 할일"></input>
         <button>추가하기</button>
       </form>
     </div>
